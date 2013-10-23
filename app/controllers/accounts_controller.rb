167d3140f41effce1fdbfb237eb6a771e9f00e97
alkp <apasenko@gmail.com>
2013-10-23 15:49:01 +0300

    ...

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,32 +1,57 @@
 class AccountsController < ApplicationController
   def new
     @account = Account.new()
+    @shop_list = Shop.all
+    @product_list = Account.all.count(group: :title)
   end
 
   def create
-    @account = Account.new(account_params)
-    if (@account.save)
-      redirect_to accounts_path
+    @shop = Shop.where(:name => params[:account][:shop_name])
+    if @shop.count == 1
+      #@account = Account.new(account_params)
+      @account = Account.new(:title => params[:account][:title], :amount => params[:account][:amount],:shop_id => @shop.first.id)
+      @shop = Shop.where(:name => params[:account][:shop_name])
+      if (@account.save)
+        redirect_to accounts_path
+      else
+        render 'new'
+      end
+
     else
-      render 'new'
+      flash.now.alert = "Укажите корректный магазин, если его нет в списке, предварительно заведите его."
+      render 'edit'
     end
+
   end
 
   def edit
     @account = Account.find(params[:id])
+    @shop_list = Shop.all
+    @product_list = Account.all.count(group: :title)
+    @shop_name = Shop.find(@account.shop_id).name if !@account.shop_id.nil?
   end
 
   def update
-    puts "suka " * 50
-    puts params[:account][:shop_name]
     @account = Account.find(params[:id])
+    @shop_list = Shop.all
+    @product_list = Account.all.count(group: :title)
 
-    if @account.update(account_params)
-      #render 'edit'
-      redirect_to accounts_path
+    @shop = Shop.where(:name => params[:account][:shop_name])
+
+    if @shop.count == 1
+      @shop_name = @shop.name
+
+      @account = Account.find(params[:id])
+      if @account.update(:title => params[:account][:title], :amount => params[:account][:amount],:shop_id => @shop.first.id)
+        redirect_to accounts_path
+      else
+        render 'edit'
+      end
     else
+      flash.now.alert = "Укажите корректный магазин, если его нет в списке, предварительно заведите его."
       render 'edit'
     end
+
   end
 
   def show
@@ -37,9 +62,16 @@ class AccountsController < ApplicationController
     @accounts = Account.all
   end
 
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to accounts_path
+  end
+
+
   private
   def account_params
-    params.require(:account).permit(:title, :amount, :date_amount)
+    params.require(:account).permit(:title, :amount, :date_amount, :shop_id)
   end
 
 end
